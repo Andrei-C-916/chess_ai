e291f028aabf5d68badd9d3e9be4a49f91ad1a0c
Andrei-C-916 <andrei.codreanu916@gmail.com>
2025-12-04 16:57:57 -0500

    implemented MCTS

--- mcts/mcts.py
+++ mcts/mcts.py
@@ -2,6 +2,7 @@ from game.chess_module import ChessModule
 from chess import Board
 import torch
 import numpy as np
+import math
 
 class Node:
     def __init__(self, game: ChessModule, args: dict, state: Board, parent=None, action_taken=None, prior=0):
@@ -11,9 +12,7 @@ class Node:
         self.parent = parent
         self.action_taken = action_taken
         self.prior = prior
-
         self.children = []
-
         self.visit_count = 0
         self.value_sum = 0
 
@@ -21,37 +20,46 @@ class Node:
         return len(self.children) > 0
     
     def select(self):
-        pass
+        best_child = None
+        best_ucb = -np.inf
+        for child in self.children:
+            ucb = self.get_ucb(child)
+            if ucb > best_ucb:
+                best_child = child
+                best_ucb = ucb
+        return best_child
 
     def get_ucb(self, child):
-        pass
+        if child.visit_count == 0:
+            q_value = 0
+        else:
+            q_value = 1 - ((child.value_sum / child.visit_count) + 1) / 2
+        return q_value + self.args['C'] * (math.sqrt(self.visit_count) / (child.visit_count + 1)) * child.prior
 
     def expand(self, policy):
         for action, prob in enumerate(policy):
             if prob > 0:
                 child_state = self.state.copy()
                 child_state = self.game.get_next_state(child_state, action)
-                
                 child = Node(self.game, self.args, child_state, self, action, prob)
                 self.children.append(child)
 
     def backpropagate(self, value):
         self.value_sum += value
         self.visit_count += 1
-
         value = -value
         if self.parent is not None:
             self.parent.backpropagate(value)
 
 class MCTS:
-    def __init__(self, game, args, model):
+    def __init__(self, game: ChessModule, args, model):
         self.game = game
         self.args = args
         self.model = model
 
     @torch.no_grad()
     def search(self, state: Board):
-        root = Node(self.args, state)
+        root = Node(self.game, self.args, state)
 
         for _ in range(self.args['num_searches']):
             node = root
@@ -59,22 +67,23 @@ class MCTS:
             while node.is_fully_expanded():
                 node = node.select()
 
-            is_terminal, value = ChessModule.check_termination_and_get_value(node.state)
+            is_terminal, value = self.game.check_termination_and_get_value(node.state)
 
             if not is_terminal:
                 policy, value = self.model(
-                    torch.tensor(ChessModule.get_state_encoding(node.state)).unsqueeze(0) #FIX
+                    torch.tensor(self.game.get_state_encoding(node.state)).unsqueeze(0)
                 )
-                policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy() #FIX
-                valid_moves = ChessModule.get_valid_moves(node.state) #FIX
+                policy = torch.softmax(policy, dim=1).squeeze(0).cpu().numpy()
+                valid_moves = self.game.get_valid_moves(node.state)
                 policy *= valid_moves
-                policy /= np.sum(policy)
-
-                value = value.item()
+                sum_policy = np.sum(policy)
+                if sum_policy > 0:
+                    policy /= sum_policy
+                else:
+                    policy = valid_moves / np.sum(valid_moves)
                 node.expand(policy)
 
             node.backpropagate(value)
-
 
         action_probs = np.zeros(self.game.action_size)
         for child in root.children:
