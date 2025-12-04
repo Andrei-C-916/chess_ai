73138757fd45694ab3eff4049c6a72a77aab74a4
Andrei-C-916 <andrei.codreanu916@gmail.com>
2025-12-03 22:41:48 -0500

    clean

--- game/auxillary.py
+++ game/auxillary.py
@@ -4,8 +4,6 @@ from chess import Board
 from typing import List
 import chess
 
-#returns a tensor representation of a chess board. Tensor is of shape (13,8,8)
-#requires: board is of type Board
 def board_to_tensor(board: Board):
     tensor = np.zeros((13,8,8))
     piece_map = board.piece_map()
@@ -23,43 +21,19 @@ def board_to_tensor(board: Board):
 
     return tensor
 
-#returns an np.array of board tensors and an np.array of labels, where the board tensors are (13,8,8) and the labels are uci formatted strings.
-#label y_i is the move that was played in position X_i
-#requires: games is of type List[Game]
-def games_to_input(games: List[Game]):
-    X = []
-    y = []
-    for game in games:
-        board = game.board() 
-        for move in game.mainline_moves():
-            X.append(board_to_tensor(board))
-            y.append(move.uci())
-            board.push(move)
-    return np.array(X, dtype=np.float32), np.array(y)
+def generate_all_moves():
+    moves = set()
+    board = chess.Board()
 
-#returns an np.array of moves encoded as ints, a dict mapping moves to ints, and a dict mapping ints to moves.
-#requires: moves is a list of uci formatted strings
-def encode_moves(moves):
-    unique_moves = list(set(moves))
-    move_to_int = {move: int for int, move in enumerate(unique_moves)}
-    int_to_move = {int: move for int, move in enumerate(unique_moves)}
-    moves = [move_to_int[move] for move in moves]
-    return np.array(moves, dtype=np.float32), move_to_int, int_to_move
+    for from_sq in chess.SQUARES:
+        for to_sq in chess.SQUARES:
+            for promo in [None, chess.QUEEN, chess.ROOK, chess.BISHOP, chess.KNIGHT]:
+                try:
+                    move = chess.Move(from_sq, to_sq, promotion=promo)
+                    if move in board.pseudo_legal:
+                        moves.add(move.uci())
+                except:
+                    pass
 
-def fen_and_moves_to_input(fen,moves):
-    X = []
-    y = [] 
-    board = Board(fen)
-    uci_list = moves.split()
-    board.push(chess.Move.from_uci(uci_list[0]))
-    uci_list = uci_list[1:]
-    for uci in uci_list:
-        tensor = board_to_tensor(board)
-        label = uci
-        X.append(tensor)
-        y.append(label)
-        board.push(chess.Move.from_uci(uci))
-    return np.array(X, dtype=np.float32), np.array(y)
-
-
+    return sorted(list(moves))
 
---
+++ models/alpha_resnet.py
@@ -0,0 +1,57 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ResNet(nn.Module):
+    def __init__(self, game, num_input_channels, num_hidden_channels, num_resBlocks, kernel_size=3, padding=1):
+        super().__init__()
+        self.startBlock = nn.Sequential(
+            nn.Conv2d(num_input_channels, num_hidden_channels, kernel_size, padding),
+            nn.BatchNorm2d(num_hidden_channels),
+            nn.ReLU()
+        )
+
+        self.backBone = nn.ModuleList(
+            [ResBlock(num_hidden_channels) for i in range(num_resBlocks)]
+        )
+
+        self.policyHead = nn.Sequential(
+            nn.Conv2d(num_hidden_channels, 32, kernel_size, padding),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(32 * game.row_count * game.column_count, game.action_size)
+        )
+
+        self.valueHead = nn.Sequential(
+            nn.Conv2d(num_hidden_channels, num_input_channels, kernel_size, padding),
+            nn.BatchNorm2d(num_input_channels),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(num_input_channels * game.row_count * game.column_count, 1),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.startBlock(x)
+        for resBlock in self.backBone:
+            x = resBlock(x)
+        policy = self.policyHead(x)
+        value = self.valueHead(x)
+        return policy, value
+    
+class ResBlock(nn.Module):
+    def __init__(self, num_hidden_channels, kernel_size=3, padding=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(num_hidden_channels, num_hidden_channels, kernel_size, padding)
+        self.bn1 = nn.BatchNorm2d(num_hidden_channels)
+        self.conv2 = nn.Conv2d(num_hidden_channels, num_hidden_channels, kernel_size, padding)
+        self.bn2 = nn.BatchNorm2d(num_hidden_channels)
+
+    def forward(self, x):
+        residual = x
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.bn2(self.conv2(x))
+        x += residual
+        x = F.relu(x)
+        return x
